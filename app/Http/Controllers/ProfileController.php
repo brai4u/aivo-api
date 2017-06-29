2a98505dd4c64d54ed7c5ea0e3d46334c2c8d383
Brian Ardiles <ardiles.brian@gmail.com>
2017-06-28 23:32:04 -0300

    created controller and method to get profile data from a fb id

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use SammyK;
+
+class ProfileController extends Controller
+{
+		/**
+		 * Create a new controller instance.
+		 *
+		 * @return void
+		 */
+		
+		public function getProfile(SammyK\LaravelFacebookSdk\LaravelFacebookSdk $fb, $id){
+
+			try {
+				$response = $fb->get('/' . $id . '?fields=' . env('FACEBOOK_SCOPE'), env('FACEBOOK_TOKEN'));
+			} catch(\Facebook\Exceptions\FacebookSDKException $e) {
+					return response()->json(['error' => $e->getMessage()]);
+			}				
+				$userData = $response->getGraphUser();
+				return response()->json(['id' => $userData->getId(),
+																	'firstName' => $userData->getFirstName(),
+																	'lastName' => $userData->getLastName(),
+																	'shortName' => $userData['short_name'],
+																	'profileImage' => $userData['picture']['url'],
+																	'coverImage' => $userData['cover']['source']
+																]);
+		}
+}
